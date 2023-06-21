c7302d23ddc729af9330f283cf49a83f3ac283be
Shobhit Singh <shobhitsingh301@gmail.com>
2023-06-21 16:03:28 +0530

    Update Ultrasonic-Relay.ino

--- Ultrasonic-Relay/Ultrasonic-Relay.ino
+++ Ultrasonic-Relay/Ultrasonic-Relay.ino
@@ -1,40 +1,34 @@
-/*
- * This example code is in the public domain *
- */
 
-// constants won't change
-const int TRIG_PIN  = 7;  // Arduino pin connected to Ultrasonic Sensor's TRIG pin
-const int ECHO_PIN  = 6;  // Arduino pin connected to Ultrasonic Sensor's ECHO pin
-const int RELAY_PIN = A5; // Arduino pin connected to Relay's pin
-const int DISTANCE_THRESHOLD = 50; // centimeters
+const int TRIG_PIN  = 7;
+const int ECHO_PIN  = 6;
+const int RELAY_PIN = A5;
+const int DISTANCE_THRESHOLD = 50;
 
-// variables will change:
 float duration_us, distance_cm;
 
 void setup() {
-  Serial.begin (9600);        // initialize serial port
-  pinMode(TRIG_PIN, OUTPUT);  // set arduino pin to output mode
-  pinMode(ECHO_PIN, INPUT);   // set arduino pin to input mode
-  pinMode(RELAY_PIN, OUTPUT); // set arduino pin to output mode
+  Serial.begin (9600);       
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);  
+  pinMode(RELAY_PIN, OUTPUT);
 }
 
 void loop() {
-  // generate 10-microsecond pulse to TRIG pin
+ 
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-  // measure duration of pulse from ECHO pin
+ 
   duration_us = pulseIn(ECHO_PIN, HIGH);
-  // calculate the distance
+ 
   distance_cm = 0.017 * duration_us;
 
   if(distance_cm < DISTANCE_THRESHOLD)
-    digitalWrite(RELAY_PIN, HIGH); // turn on Relay
+    digitalWrite(RELAY_PIN, HIGH);
   else
-    digitalWrite(RELAY_PIN, LOW);  // turn off Relay
+    digitalWrite(RELAY_PIN, LOW);
 
-  // print the value to Serial Monitor
   Serial.print("distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
